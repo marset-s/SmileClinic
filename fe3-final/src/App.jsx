f8d5157ce51adf5a1d1d49fa7a04874fbf51bc7b
Mariangeles2103 <angelesilvac@gmail.com>
2023-09-07 17:42:37 -0300

    Se agregaron pages

--- fe3-final/src/App.jsx
+++ fe3-final/src/App.jsx
@@ -2,15 +2,29 @@
 import './App.css'
 import Footer from './Components/Footer'
 import Navbar from './Components/Navbar'
+import { Routes, Route } from 'react-router-dom'
+import Inicio from './Components/Pages/Inicio'
+import Contacto from './Components/Pages/Contacto'
+import Detalle from './Components/Pages/Detalle'
+import Destacados from './Components/Pages/Destacados'
 
 function App() {
  
 
   return (
-    <div className="App">
-          <Navbar/>
-          <Footer/>
-      </div>
+
+    <> 
+            
+      <Navbar/>
+      <Routes>      
+        <Route path='/' element={<Inicio/>}/>
+        <Route path='/contact' element={<Contacto />}/>
+        <Route path='/dentist/:id' element={<Detalle />}/>
+        <Route path='/fav' element={<Destacados />}/>
+      </Routes>
+      <Footer/>     
+      
+    </>
   )
 }
 
---
+++ fe3-final/src/Components/Pages/Destacados.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Destacados = () => {
+  return (
+    <div>Destacados</div>
+  )
+}
+
+export default Destacados
