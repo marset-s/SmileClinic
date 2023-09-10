d0395d2d4f7b5383d8a59b8b6c735f236ce890ff
Serrana Marset <serranamarset@gmail.com>
2023-09-10 00:25:36 -0300

    Configuración del contexto global

--- fe3-final/src/Components/utils/global.context.jsx
+++ fe3-final/src/Components/utils/global.context.jsx
@@ -1,15 +1,23 @@
-import { createContext } from "react";
+import { createContext, useContext } from "react";
 
 export const initialState = {theme: "", data: []}
 
-export const ContextGlobal = createContext(undefined);
+export const ContextGlobal = createContext(initialState);
 
 export const ContextProvider = ({ children }) => {
   //Aqui deberan implementar la logica propia del Context, utilizando el hook useMemo
 
+
+
+  
+
   return (
-    <ContextGlobal.Provider value={{}}>
+    <ContextGlobal.Provider value={{theme, data}}>
       {children}
     </ContextGlobal.Provider>
   );
 };
+
+export default ContextProvider
+
+export const useContextGlobal = () => useContext(ContextGlobal)
--- fe3-final/src/main.jsx
+++ fe3-final/src/main.jsx
@@ -3,5 +3,9 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { BrowserRouter} from 'react-router-dom'
+import ContextProvider from './Components/utils/global.context.jsx'
 
-ReactDOM.createRoot(document.getElementById('root')).render(<BrowserRouter><App /></BrowserRouter>)
+ReactDOM.createRoot(document.getElementById('root')).render
+(<ContextProvider>
+<BrowserRouter><App /></BrowserRouter>
+</ContextProvider>)
